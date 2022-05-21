1fae3dccea71ee759fe598ff71645ddae758df08
kjh42447 <kjh42447@naver.com>
2022-05-21 23:48:14 +0900

    (2-4)템플릿

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from multiprocessing import context
+#from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Question
 
-#브라우저에 해당 문자열 출력
+#브라우저 출력
+#질문 목록 화면
 def index(request):
-    return HttpResponse("안녕하세요 myapp에 오신것을 환영합니다.")
+    question_list = Question.objects.order_by('-create_date')
+    context = {"question_list" : question_list}
+    return render(request, "myapp/question_list.html", context)
+
+#질문 상세 내용 화면
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {"question": question}
+    return render(request, "myapp/question_detail.html", context)
